1c1ac810bf020b716fa1d10d01f85d0207d4537b
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-26 12:51:00 +0530

    ch 2 completed

--- challenge-2/app.js
+++ challenge-2/app.js
@@ -1,3 +1,32 @@
 /**
  * Write your challenge solution here
  */
+
+const redButton = document.getElementById('redButton')
+const greenButton = document.getElementById('greenButton')
+const blueButton = document.getElementById('blueButton')
+const purpleButton = document.getElementById('purpleButton')
+const resetButton = document.getElementById('resetButton')
+const mainHead = document.getElementById('mainHeading')
+
+redButton.addEventListener('click', () =>{
+    mainHead.style.color = '#e74c3c'
+})
+
+greenButton.addEventListener('click', () =>{
+    mainHead.style.color = '#2ecc71'
+})
+
+blueButton.addEventListener('click', () =>{
+    mainHead.style.color = '#3498db'
+})
+
+purpleButton.addEventListener('click', () =>{
+    mainHead.style.color = '#9b59b6'
+})
+
+resetButton.addEventListener('click', () =>{
+    mainHead.style.color = '#34495e'
+})
+
+
