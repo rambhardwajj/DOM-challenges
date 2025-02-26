8077630c4fb7a2c86356a70223a39fc2240a9fc0
rambhardwajj <rambhardwaj101000@gmail.com>
2025-02-26 12:30:23 +0530

    challenge 1 completed

--- challenge-1/app.js
+++ challenge-1/app.js
@@ -2,3 +2,27 @@
  * Write your challenge solution here
  */
 
+const toggleBtn = document.getElementById("toggleButton")
+const bulb = document.getElementById('bulb')
+const statusSign = document.getElementById('status')
+const body = document.getElementById('body')
+
+toggleBtn.addEventListener('click', ()=>{
+    let currStatus = toggleBtn.innerText;
+    console.log(currStatus)
+    if( currStatus == 'Turn On'){
+        bulb.classList.remove('off')
+        toggleBtn.innerText = "Turn Off"
+
+        body.classList.add('dark-mode')
+
+        statusSign.innerText = 'Status On'
+    }else{
+        bulb.classList.add('off')
+        toggleBtn.innerText = "Turn On"
+
+        body.classList.remove('dark-mode')
+
+        statusSign.innerText = 'Status Off'
+    }
+})
