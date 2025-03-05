da270245e71b8a0624dc144a6174fcc0bf2abd49
rambhardwajj <rambhardwaj101000@gmail.com>
2025-03-05 14:03:33 +0530

    edit functionality added

--- Kanban/index.js
+++ Kanban/index.js
@@ -5,12 +5,11 @@ const overLay           = document.querySelector('.overlay')
 const submitBtn         = document.getElementById('Submit')
 
 const promptTitle       = document.getElementById('prompt-title')
-const promptName        = document.getElementById('prompt-name') 
 const promptDesc        = document.getElementById('prompt-desc') 
 const todoBoard         = document.getElementById('to-do')       
 let buttonPriorityIcon  = '🟢';
 
-
+let editingTask = null; 
 
 createTaskBtn.addEventListener('click', ()=>{
     prompt.classList.add('active');
@@ -19,56 +18,85 @@ createTaskBtn.addEventListener('click', ()=>{
 
 submitBtn.addEventListener('click', ()=>{
     const currPromptTitle = promptTitle.value;       // console.log(currPromptTitle)
-    const currPromptName  = promptName.value;        // console.log(currPromptName)
     const currPromptDesc  = promptDesc.value;        // console.log(currPromptDesc)
     
-    if( currPromptDesc =='' || currPromptTitle ==''|| currPromptName==''){
-        alert('Please complete the form')
+    if( currPromptDesc =='' || currPromptTitle ==''){
+        alert('Please complete the form'); 
         return;
     }
-    const taskDiv = document.createElement('div')
-    taskDiv.classList.add('task')
 
-    // Priority
-    const taskPriorityDiv = document.createElement('span')
-    taskPriorityDiv.innerText = buttonPriorityIcon
-    // Title
-    const taskTitleDiv = document.createElement('div')
-    taskTitleDiv.innerText = currPromptTitle;
-    taskTitleDiv.id = 'task-title-div'
-    taskTitleDiv.appendChild(taskPriorityDiv)
-    // Desc
-    const taskDescDiv = document.createElement('div')
-    taskDescDiv.innerText = currPromptDesc;
-    taskDescDiv.id = 'task-desc-div'
+    if( editingTask){
+        console.log('editing')
+        let titleDiv = editingTask.querySelector('#task-title-div');
+        let descDiv = editingTask.querySelector('#task-desc-div');
 
-    // task div mai add kro
-    taskDiv.appendChild(taskTitleDiv)
-    taskDiv.appendChild(taskDescDiv)
-    taskDiv.draggable = true;
+        titleDiv.innerHTML = ''; // icon ko bhi hatane ke liye html ko ' ' banao
+        titleDiv.innerText = currPromptTitle;
 
-    attachDrag(taskDiv)
+        let taskPriorityDiv = document.createElement('span')
+        taskPriorityDiv.innerText = buttonPriorityIcon;
+        titleDiv.appendChild(taskPriorityDiv);
 
-    todoBoard.appendChild(taskDiv)
+        descDiv.innerText = currPromptDesc;
+        editingTask = null;
 
+    } else if( editingTask == null){
+        // Creating anew task
+        const taskDiv = document.createElement('div')
+        taskDiv.classList.add('task')
+    
+        // Priority
+        const taskPriorityDiv = document.createElement('span')
+        taskPriorityDiv.innerText = buttonPriorityIcon
+        // Title
+        const taskTitleDiv = document.createElement('div')
+        taskTitleDiv.innerText = currPromptTitle;
+        taskTitleDiv.id = 'task-title-div'
+        taskTitleDiv.appendChild(taskPriorityDiv)
+        // Desc
+        const taskDescDiv = document.createElement('div')
+        taskDescDiv.innerText = currPromptDesc;
+        taskDescDiv.id = 'task-desc-div'
+    
+        // task div mai add kro
+        taskDiv.appendChild(taskTitleDiv)
+        taskDiv.appendChild(taskDescDiv)
+        taskDiv.draggable = true;
+    
+        attachDrag(taskDiv)
+        addEditOption(taskDiv)
+    
+        todoBoard.appendChild(taskDiv)
+    }
     cancelBtn()
 })
 
-
 const allBoards = document.querySelectorAll('.board')     
-const allTasks = document.querySelectorAll('.task')
+const allTasks  = document.querySelectorAll('.task')
 
-function attachDrag(taskDiv){
-    taskDiv.addEventListener('dragstart', () => {
-        taskDiv.classList.add('flying')
-    })
-    taskDiv.addEventListener('dragend', () => {
-
-        taskDiv.classList.remove('flying')
-    })
-}
 
 allTasks.forEach((item)=> attachDrag(item))
+allTasks.forEach((item)=> addEditOption(item))
+
+// edit functionality 
+function addEditOption(taskDiv){
+    taskDiv.addEventListener('click', (event)=> updateTask(event.currentTarget))
+}
+function updateTask(taskDiv){
+    console.log(taskDiv)
+    editingTask = taskDiv;
+    
+    let currDesc = taskDiv.querySelector('#task-desc-div');
+    let currTitle = taskDiv.querySelector('#task-title-div');
+
+    prompt.classList.add('active');
+    overLay.classList.add('active')
+    
+    promptTitle.value = currTitle.childNodes[0].textContent.trim();
+    promptDesc.value = currDesc.innerText;
+}
+
+// Dragg functionality 
 allBoards.forEach((board)=>{
     board.addEventListener('dragover', (event)=>{
         event.preventDefault();
@@ -78,7 +106,7 @@ allBoards.forEach((board)=>{
         board.appendChild(flyingEl)
     });
 })
-
+// add radio icons
 function toggleRadio(buttonPriority){
     if( buttonPriority == 'low'){
         buttonPriorityIcon = '🟢'
@@ -90,10 +118,20 @@ function toggleRadio(buttonPriority){
         buttonPriorityIcon = '💠';
     }
 }
+// remove prompt visiblity 
 function cancelBtn(){
     prompt.classList.remove('active');
     overLay.classList.remove('active');
     promptTitle.value= ''
     promptDesc.value = ''
-    promptName.value = ''
+
+    buttonPriorityIcon  = '🟢'
+}
+function attachDrag(taskDiv){
+    taskDiv.addEventListener('dragstart', () => {
+        taskDiv.classList.add('flying')
+    })
+    taskDiv.addEventListener('dragend', () => {
+        taskDiv.classList.remove('flying')
+    })
 }
